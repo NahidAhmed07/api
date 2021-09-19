1514b5cff9afd1c1db1f3184f00405184cee3f87
NahidAhmed07 <mdnahidahmed2002@gmail.com>
2021-09-19 14:42:31 +0600

    first commit

---
+++ api.js
@@ -0,0 +1,20 @@
+const players = [
+  {
+    id:1,
+    images: "https://github.com/NahidAhmed07/responsive-football/blob/main/images/players/player-1.png",
+    name: "Neymar Jr",
+    dis:"Neymar came into prominence at Santos, where he made his professional debut aged 17."
+  },
+  {
+    id:2,
+    images: "https://github.com/NahidAhmed07/responsive-football/blob/main/images/players/player-10.png",
+    name: "Harry Kane",
+    dis:"Harry Edward Kane MBE (born 28 July 1993) is an English professional footballer who play."
+  },
+  {
+    id:3,
+    images: "https://github.com/NahidAhmed07/responsive-football/blob/main/images/players/player-1.png",
+    name: "Neymar Jr",
+    dis:"Neymar came into prominence at Santos, where he made his professional debut aged 17."
+  }
+]
