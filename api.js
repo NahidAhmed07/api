2003a0624d48f2b4fabf0fa4f9835cc0ff860b45
NahidAhmed07 <mdnahidahmed2002@gmail.com>
2021-10-04 07:11:15 +0600

    services data added

--- api.js
+++ api.js
@@ -1,20 +1,14 @@
-const players = [
-  {
-    id:1,
-    images: "https://github.com/NahidAhmed07/responsive-football/blob/main/images/players/player-1.png",
-    name: "Neymar Jr",
-    dis:"Neymar came into prominence at Santos, where he made his professional debut aged 17."
-  },
-  {
-    id:2,
-    images: "https://github.com/NahidAhmed07/responsive-football/blob/main/images/players/player-10.png",
-    name: "Harry Kane",
-    dis:"Harry Edward Kane MBE (born 28 July 1993) is an English professional footballer who play."
-  },
-  {
-    id:3,
-    images: "https://github.com/NahidAhmed07/responsive-football/blob/main/images/players/player-1.png",
-    name: "Neymar Jr",
-    dis:"Neymar came into prominence at Santos, where he made his professional debut aged 17."
-  }
+const players=[
+    {name: 'Neymar Jr', imgage:'https://github.com/Hasibul677/responsive-football/blob/main/images/players/player-1.png?raw=true', details:'Neymar, is a Brazilian professional footballer who plays as a forward for Ligue 1 club Paris.'},
+    {name: 'Lionel Messi', imgage:'https://hasibul677.github.io/responsive-football/images/players/player-2.png', details:'Leo Messi is Argentine professional footballer who plays as a forward or an attacking midfielder and captains.'},
+    {name: 'Cristiano Ronaldo', imgage:'https://hasibul677.github.io/responsive-football/images/players/player-3.png', details:'Cristiano Ronaldo is a Portuguese professional footballer who plays as a forward for Serie A club Juventus and captain.'},
+    {name: 'Paulo Dybala', imgage:'https://hasibul677.github.io/responsive-football/images/players/player-4.png', details:'Paulo Dybala Argentine professional footballer who plays as a forward for Serie A club Juventus and the Argentina.'},
+    {name: 'Mesut Ozil', imgage:'https://hasibul677.github.io/responsive-football/images/players/player-5.png', details:'Mesut Özil is a German professional footballer who plays as an attacking midfielder for Süper Lig club Fenerbahçe.'},
+    {name: 'Mauro Icardi', imgage:'https://hasibul677.github.io/responsive-football/images/players/player-6.png', details:'Mauro Icardi (born 19 February 1993) is an Argentine professional footballer.'},
+    {name: 'Di María', imgage:'https://hasibul677.github.io/responsive-football/images/players/player-7.png', details:'Di María is Argentine professional footballer who plays for Ligue 1 club Paris Saint-Germain and the Argentina.'},
+    {name: 'Mohamed Salah', imgage:'https://hasibul677.github.io/responsive-football/images/players/player-8.png', details:'Kylian Mbappé is a French professional footballer who plays as a forward for Ligue 1 club Paris Saint-Germain.'},
+    {name: 'Harry Edward', imgage:'https://hasibul677.github.io/responsive-football/images/players/player-9.png', details:'Mohamed Salah Hamed Mahrous Ghaly is an Egyptian professional footballer who plays as a forward for Premier League club Liverpool.'}
+    
 ]
+  
+console.log(JSON.stringify(players));
